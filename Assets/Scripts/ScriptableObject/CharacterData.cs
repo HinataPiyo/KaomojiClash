c0b78ad933e1eea28d1b6f3b677ee2211f25d91c
Hinata <tairo8291@gmail.com>
2025-12-13 11:57:23 +0900

    PlayerDataとEnemyDataのクラスを作成

--- Assets/Scripts/ScriptableObject/CharacterData.cs
+++ Assets/Scripts/ScriptableObject/CharacterData.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 using Constants.Global;
 
-[CreateAssetMenu(fileName = "CharacterData", menuName = "CharacterData", order = 0)]
 public class CharacterData : ScriptableObject
 {
     [SerializeField]CharacterStatus status;
