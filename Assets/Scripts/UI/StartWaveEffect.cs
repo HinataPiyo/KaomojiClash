bfc694b8cf19acf8fe5b7991cf628edb8b2c124c
Hinata <tairo8291@gmail.com>
2026-01-08 04:08:31 +0900

    feat: 新しい音声イベントを追加し、スタートウェーブエフェクトの音声再生機能を実装

---
+++ Assets/Scripts/UI/StartWaveEffect.cs
@@ -0,0 +1,15 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class StartWaveEffect : MonoBehaviour
+{
+    public void ReadyPlaySE()
+    {
+        AudioManager.I.PlaySE("READY");
+    }
+
+    public void FightPlaySE()
+    {
+        AudioManager.I.PlaySE("FIGHT");
+    }
+}
