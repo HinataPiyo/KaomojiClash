6bc8e98fc8d01fc38a72d13d73a37d6d2f2cc137
HinataPiyo <174671473+HinataPiyo@users.noreply.github.com>
2026-01-17 22:26:52 +0900

    Ipadでコード編集してみた

--- Assets/Scripts/Constants/Layer.cs
+++ Assets/Scripts/Constants/Layer.cs
@@ -3,4 +3,5 @@ public static class Layer
     public const string BATTLE = "Battle";
     public const string WORLD = "World";
     public const string Player = "Player";
+    public const string WALL = "Wall";
 }
