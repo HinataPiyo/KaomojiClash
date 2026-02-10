f62b72be2ed39e5a4c2b623fcd9f99ad4b5cbea7
Hinata <tairo8291@gmail.com>
2026-02-11 03:48:21 +0900

    エリアの追加と修正/AreaManagerクラスの修正

--- Assets/Scripts/Area/AreaManager.cs
+++ Assets/Scripts/Area/AreaManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class AreaManager : MonoBehaviour
 {
     public static AreaManager I { get; private set; }
-    [SerializeField] AreaData[] areaDatas;    public AreaData[] AreaDatas => areaDatas;
+    [SerializeField] AreaDatabase areaDatabase;    public AreaDatabase AreaDatabase => areaDatabase;
     public AreaData CurrentAreaData { get; private set; }
 
     void Awake()
@@ -18,19 +18,17 @@ public class AreaManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-
-        SetCurrentAreaData(0);
     }
 
     public void SetCurrentAreaData(int index)
     {
-        if(index < 0 || index >= areaDatas.Length)
+        if(index < 0 || index >= areaDatabase.GetAllAreas().Length)
         {
             Debug.LogError("Invalid area index: " + index);
             return;
         }
 
-        CurrentAreaData = areaDatas[index];
+        CurrentAreaData = areaDatabase.GetAllAreas()[index];
     }
 
     /// <summary>
